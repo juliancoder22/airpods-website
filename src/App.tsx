cf4b81bebf4f670b532da2b3f1de4c422db7f73f
Julian <julian.andres.aquino33@gmail.com>
2024-05-29 20:38:01 -0300

    feature header created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import "./index.css";
 
 function App() {
   return (
     <>
+      <Header />
       <Hero />
     </>
   );
