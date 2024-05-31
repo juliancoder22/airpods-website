cd5dc8770b1069ac5bfd55127baf819846220371
Julian <julian.andres.aquino33@gmail.com>
2024-05-31 08:29:18 -0300

    feature footer created

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="flex justify-between items-center py-10 px-60">
+      <span className="text-lg font-semibold text-xl">Airpods Website</span>
+      <ul className="flex justify-center items-center gap-10">
+        <li className="cursor-pointer font-semibold">Privacy Policy</li>
+        <li className="cursor-pointer font-semibold">Terms & Conditions</li>
+        <li className="cursor-pointer font-semibold">Support</li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
