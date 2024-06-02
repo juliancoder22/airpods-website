cd540a1fac2514d41df22699f36c56dfebb0b33b
Julian <julian.andres.aquino33@gmail.com>
2024-06-02 12:24:16 -0300

    animation fix

--- src/components/QualitySound/QualitySound.tsx
+++ src/components/QualitySound/QualitySound.tsx
@@ -1,19 +1,24 @@
 import AirpodsQuality from "../../assets/airpods-quality.png";
 import AirpodsQualityBackground from "../../assets/airpods-quality-background.svg";
+import { Fade } from "react-awesome-reveal";
 
 const QualitySound = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-slate-900">
-      <h1 className="text-slate-100 text-5xl font-bold">
-        Trendy designs with <br /> better sound quality.
-      </h1>
-      <p className="text-slate-300 text-lg text-center mt-8">
-        We share common trends and strategies for <br /> improving your rental
-        income
-      </p>
-      <div className="flex justify-center gap-10 mt-10">
-        <img src={AirpodsQuality} alt="Airpod Back" className="w-full" />
-      </div>
+      <Fade direction="down" triggerOnce duration={1500}>
+        <h1 className="text-slate-100 text-5xl font-bold">
+          Trendy designs with <br /> better sound quality.
+        </h1>
+        <p className="text-slate-300 text-lg text-center mt-8">
+          We share common trends and strategies for <br /> improving your rental
+          income
+        </p>
+      </Fade>
+      <Fade direction="up" triggerOnce duration={1500}>
+        <div className="flex justify-center gap-10 mt-10">
+          <img src={AirpodsQuality} alt="Airpod Back" className="w-full" />
+        </div>
+      </Fade>
       <img src={AirpodsQualityBackground} className="absolute mt-80" />
     </div>
   );
