df284c21f96f6eef6bdfc23068f39214d25a28f8
Julian <julian.andres.aquino33@gmail.com>
2024-06-11 10:15:38 -0300

    fixes related to mobile design

--- src/components/AirpodsInfo/AirpodsInfo.tsx
+++ src/components/AirpodsInfo/AirpodsInfo.tsx
@@ -6,24 +6,22 @@ import { Fade } from "react-awesome-reveal";
 const AirpodsInfo = () => {
   return (
     <div className="flex bg-slate-100 section min-h-screen md:min-h-min">
-      {/* División para la imagen */}
       <div className="hidden md:block">
         <img src={AirpodsInfoImg} alt="" />
       </div>
-      {/* Contenido de texto */}
       <Fade direction="down" triggerOnce duration={2000}>
-        <div className="flex flex-col p-4 lg:ml-32">
-          <h2 className="text-5xl font-bold mt-40 ">
+        <div className="flex flex-col p-6 lg:ml-32">
+          <h2 className="text-center md:text-start text-5xl font-bold mt-40 ">
             Listen music <br /> anytime, anywhere.
           </h2>
-          <p className="text-lg mt-10">
-            We share common trends and strategies for{" "}
+          <p className="text-center md:text-start text-lg mt-10">
+            Explore the latest innovations and designs from{" "}
             <br className="hidden md:block" />
-            improving your rental income.
+            the new AirPods.
           </p>
           <div className="flex gap-20">
-            <div className="flex flex-col mt-10 md:mt-40 md:flex-row ">
-              <div className="flex mr-4 justify-start items-start">
+            <div className="flex flex-col mt-10 md:mt-40 md:flex-row w-1/2 text-center items-center">
+              <div className="flex md:mr-4 justify-start items-start">
                 <img
                   src={AirpodsIcon}
                   alt="airpods icon"
@@ -33,13 +31,14 @@ const AirpodsInfo = () => {
               <div className="flex flex-col">
                 <h3 className="font-bold text-lg">Comfortable Buds</h3>
                 <p className="font-light pt-2 text-sm md:pt-6 md:text-lg">
-                  With lots of unique blocks,
-                  <br /> you can easily build a page <br /> without coding.
+                  Experience surround sound,
+                  <br /> designed to adapt to your daily life <br /> without
+                  complications.
                 </p>
               </div>
             </div>
-            <div className="flex flex-col mt-10 md:mt-40 md:flex-row">
-              <div className="flex mr-4 justify-start items-start">
+            <div className="flex flex-col mt-10 md:mt-40 md:flex-row w-1/2 text-center items-center">
+              <div className="flex md:mr-4 justify-start items-start">
                 <img
                   src={SoundIcon}
                   alt="airpods icon"
@@ -49,8 +48,8 @@ const AirpodsInfo = () => {
               <div className="flex flex-col">
                 <h3 className="font-bold text-lg">Comfortable Buds</h3>
                 <p className="font-light pt-2 text-sm md:pt-6 md:text-lg">
-                  With lots of unique blocks,
-                  <br /> you can easily build a page <br /> without coding.
+                  Enjoy wireless freedom,
+                  <br /> with an all-day battery <br /> and fast charging.
                 </p>
               </div>
             </div>
